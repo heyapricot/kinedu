47dbcbdce7a3749503b96669591ea8ac840f2cd3
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-22 18:34:20 -0500

    Added status column to activity_log index table

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -4,7 +4,11 @@ class ActivityLog < ApplicationRecord
   belongs_to :activity
 
   def duration
-    (stop_time - start_time)/60
+    (stop_time - start_time)/60 unless stop_time.nil?
+  end
+
+  def status
+    stop_time.nil? ? "En Progreso" : "Terminada"
   end
 
 end
