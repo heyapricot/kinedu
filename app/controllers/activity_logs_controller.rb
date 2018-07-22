6139b64f13ed852cb480f278c8cb8b901abe3baf
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-22 15:37:51 -0500

    Enabled GET index for activity_logs with controller and an empty view. Deleted unused controller specs

---
+++ app/controllers/activity_logs_controller.rb
@@ -0,0 +1,5 @@
+class ActivityLogsController < ActionController::Base
+  def index
+
+  end
+end
--- spec/controllers/api/activities_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Api::ActivitiesController, type: :controller do
-
-end
