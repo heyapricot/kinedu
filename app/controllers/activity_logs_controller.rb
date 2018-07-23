13f3e1f098ce3867063177f44ddf33ca3c94672e
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-23 17:42:22 -0500

    Added status filter to Activity Log index view

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -3,7 +3,7 @@ class ActivityLogsController < ActionController::Base
     query = Hash.new
     query[:baby_id] = params[:baby_id] unless params[:baby_id].blank?
     query[:assistant_id] = params[:assistant_id] unless params[:assistant_id].blank?
-
+    query[:stop_time] = nil if params[:status] == "En Progreso"
     query.empty? ? @activity_logs = ActivityLog.all : @activity_logs = ActivityLog.where(query)
 
 
--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -3,10 +3,6 @@ class ActivityLog < ApplicationRecord
   belongs_to :assistant
   belongs_to :activity
 
-  def duration
-    (stop_time - start_time)/60 unless stop_time.nil?
-  end
-
   def status
     stop_time.nil? ? "En Progreso" : "Terminada"
   end
