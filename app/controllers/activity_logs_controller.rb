3709f2ed43324a62c6824f9539ae7bbc561663da
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-23 16:58:29 -0500

    Added working filter dropdowns to ActivityLog index view

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -1,5 +1,12 @@
 class ActivityLogsController < ActionController::Base
   def index
-    @activity_logs = ActivityLog.all
+    query = Hash.new
+    query[:baby_id] = params[:baby_id] unless params[:baby_id].blank?
+    query[:assistant_id] = params[:assistant_id] unless params[:assistant_id].blank?
+
+    query.empty? ? @activity_logs = ActivityLog.all : @activity_logs = ActivityLog.where(query)
+
+
+
   end
 end
