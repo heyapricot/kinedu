2d2646a3d1a72bb709bc5f635243dd05752e32fe
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-07-25 19:01:52 -0500

    Added date validation to activity log

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -3,6 +3,8 @@ class ActivityLog < ApplicationRecord
   belongs_to :assistant
   belongs_to :activity
 
+  validate :start_must_be_before_end_time
+
   scope :finished, ->{where("stop_time IS NOT NULL")}
   scope :in_progress, ->{where("stop_time IS NULL")}
 
@@ -16,6 +18,12 @@ class ActivityLog < ApplicationRecord
 
   def set_duration
     self.duration = (stop_time - start_time).minutes.to_i unless stop_time.nil?
+  end
+
+  private
+
+  def start_must_be_before_end_time
+    errors.add(:start_time, "must be before stop time") if !stop_time.nil? && start_time > stop_time
   end
 
 end
